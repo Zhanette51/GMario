465ec0a966e312717d13f03a7328b4d7566468ce
Zhanette <zhakat51@gmail.com>
2025-12-05 22:07:35 +0400

    Update game.js

--- game.js
+++ game.js
@@ -3,8 +3,8 @@ const CONFIG = {
     player: {
         startX: 50,
         startY: 50, // Опустили игрока (было 250)
-        width: 40,
-        height: 60,
+        width: 20,
+        height: 20,
         speed: 5,
         jumpForce: 16,
         lives: 3
